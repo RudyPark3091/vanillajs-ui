10c7135544d47daccc09c24b685d2a8ddcc611c5
RudyPark3091 <grayblack313@gmail.com>
2020-10-30 23:51:57 +0900

    draggable=true

--- draggable-grid-system/draggable-grid-system.js
+++ draggable-grid-system/draggable-grid-system.js
@@ -1,11 +1,20 @@
 (function () {
   const container = document.getElementById("dr-grid-container");
+  const gridItems = container.childNodes;
+
+  gridItems.forEach(item => {
+    let interval;
+    item.addEventListener("dragstart", (e) => {
+      const dragged = e.target;
+      dragged.style.opacity = .5;
+    })
+
+    item.ondragexit = (e) => {
+      console.log("drag exit on", e.target);
+    }
+  });
+
   const items = [];
-
-  const div = document.createElement("div");
-  div.innerText = "Hello World";
-
-  items.push(div);
 
   items.forEach(item => {
     container.appendChild(item);
