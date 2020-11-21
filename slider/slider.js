c982a79a64ce334119f1b5aae91610c434a6c2e1
RudyPark3091 <grayblack313@gmail.com>
2020-11-22 03:01:13 +0900

    Add slider

---
+++ slider/slider.js
@@ -0,0 +1,11 @@
+(function () {
+  const container = document.querySelector("#slider-container");
+  container.innerHTML += `
+    <div class="slider-item"></div>
+    <div class="slider-item"></div>
+    <div class="slider-item"></div>
+    <div class="slider-item"></div>
+    <div class="slider-item"></div>
+    <div class="slider-item"></div>
+  `;
+})();
