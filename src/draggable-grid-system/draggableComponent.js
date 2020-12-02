55d386c570b746f50c269d548439e668dda3bd48
RudyPark3091 <grayblack313@gmail.com>
2020-12-03 00:03:35 +0900

    Add drag feature
    dragging divider moves partition of screen

--- src/draggable-grid-system/draggableComponent.js
+++ src/draggable-grid-system/draggableComponent.js
@@ -3,7 +3,9 @@ class Draggable extends HTMLElement {
 		super();
 
 		const shadow = this.attachShadow({ mode: 'open' });
+		const DIVIDERWIDTH = 10;
 
+		// wrapper element
 		const wrapper = document.createElement("div");
 		wrapper.id = "draggable-wrapper";
 		wrapper.innerHTML = `
@@ -13,32 +15,41 @@ class Draggable extends HTMLElement {
 		</div>
 		<div id="draggable-item2"></div>
 		`;
+		// child elements
 		const item1 = wrapper.querySelector("#draggable-item1");
 		const item2 = wrapper.querySelector("#draggable-item2");
 		const divider = wrapper.querySelector("#draggable-divider");
 
-		let startPosX, startWidth;
+		// dragging slider logic
+		let startPosX, startWidth, item1Width, item2Width, bodyWidth;
 		divider.addEventListener("mousedown", handleMouseDown);
 		document.documentElement.addEventListener("mouseup", handleDragStop);
 
 		function handleMouseDown(e) {
 			startPosX = e.clientX;
-			startWidth = parseInt(document.defaultView.getComputedStyle(divider).width);
+			startWidth = parseInt(document.defaultView.getComputedStyle(divider).left);
+			item1Width = parseInt(document.defaultView.getComputedStyle(item1).width);
+			item2Width = parseInt(document.defaultView.getComputedStyle(item2).width);
+			bodyWidth = parseInt(document.defaultView.getComputedStyle(document.body).width);
 			document.documentElement.addEventListener("mousemove", handleDrag);
 		}
 
 		function handleDrag(e) {
-			divider.style.width = (startWidth + e.clientX - startPosX) + 'px';
+			divider.style.left = (startWidth + e.clientX - startPosX) + 'px';
+			item1.style.width = (startWidth + e.clientX - startPosX) + 'px';
+			item2.style.left = (startWidth + e.clientX + DIVIDERWIDTH - startPosX) + 'px';
+			item2.style.width = (bodyWidth - parseInt(document.defaultView.getComputedStyle(item1).width) - DIVIDERWIDTH) + 'px';
 		}
 
 		function handleDragStop(e) {
 			document.documentElement.removeEventListener("mousemove", handleDrag);
 		}
 
+		// css styling
 		const style = document.createElement("style");
 		style.textContent = `
 		#draggable-wrapper {
-			--divider-width: 10px;
+			--divider-width: ${DIVIDERWIDTH}px;
 			width: 100%;
 			height: 100%;
 			background-color: #bbbbbb;
@@ -58,16 +69,21 @@ class Draggable extends HTMLElement {
 
 		#draggable-wrapper #draggable-item2 {
 			background-color: #777777;
+			position: absolute;
+			left: calc(50% - var(--divider-width)/2 + var(--divider-width));
 		}
 
 		#draggable-wrapper #draggable-divider {
-			width: 10px;
+			width: var(--divider-width);
 			height: 100%;
 			background-color: #ffffff;
 			cursor: ew-resize;
 			display: flex;
 			justify-content: center;
 			align-items: center;
+			position: absolute;
+			left: calc(50% - var(--divider-width)/2);
+			z-index: 999;
 		}
 
 		#draggable-divider > div {
